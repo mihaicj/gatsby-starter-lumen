146537b5e3e15e801fe8566779869b48189d116f
Mihai Muresan <mihai.muresan13@gmail.com>
2025-03-13 12:28:33 +0100

    Add clarity tracking code

--- gatsby/on-render-body.js
+++ gatsby/on-render-body.js
@@ -26,6 +26,18 @@ const onRenderBody = ({ setHeadComponents, setPostBodyComponents }) => {
         __html: `(function(v,i,s,a,t){v[t]=v[t]||function(){(v[t].v=v[t].v||[]).push(arguments)};if(!v._visaSettings){v._visaSettings={}}v._visaSettings[a]={v:'1.0',s:a,a:'1',t:t};var b=i.getElementsByTagName('body')[0];var p=i.createElement('script');p.defer=1;p.async=1;p.src=s+'?s='+a;b.appendChild(p)})(window,document,'//dev-worker.va-endpoint.com/main.js','06d99a32-ee11-11eb-830a-0242ac130018','va')`,
       }}
     />,
+  ], [
+      <script
+      key="visa"
+      type="text/javascript"
+      dangerouslySetInnerHTML={{
+        __html: `(function(c,l,a,r,i,t,y){
+          c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+          t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+          y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+      })(window, document, "clarity", "script", "qnjuh2svqt");`
+      }}
+      />
   ]);
 };
 
